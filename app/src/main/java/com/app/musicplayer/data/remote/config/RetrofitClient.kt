c18f371dbe97b4a55a050cf650a17d8287880709
devDedeo <93327877+Dedeo3@users.noreply.github.com>
2026-09-05 20:19:43 +0700

    fix: outside network module for di

--- app/src/main/java/com/app/musicplayer/data/remote/config/RetrofitClient.kt
+++ app/src/main/java/com/app/musicplayer/data/remote/config/RetrofitClient.kt
@@ -7,22 +7,21 @@ import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-object RetrofitClient {
-    val networkModule = module {
-        val url = "https://itunes.apple.com/"
-        single {
-            HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
-        }
-        single {
-            OkHttpClient.Builder().addInterceptor(get<HttpLoggingInterceptor>()).build()
-        }
-        single {
-            Retrofit.Builder()
-                .baseUrl(url)
-                .client(get())
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-                .create(ApiService::class.java)
-        }
+private const val BASE_URL = "https://itunes.apple.com/"
+
+val networkModule = module {
+    single {
+        HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
+    }
+    single {
+        OkHttpClient.Builder().addInterceptor(get<HttpLoggingInterceptor>()).build()
+    }
+    single {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(get())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiService::class.java)
     }
 }
